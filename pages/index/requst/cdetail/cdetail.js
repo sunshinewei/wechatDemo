eeafcd3293d7381a874ad8cc5505f4132e9a1e6a
sunshienwei <1436699184@qq.com>
2018-07-26 18:11:01 +0800

    条目内容

--- pages/index/requst/cdetail/cdetail.js
+++ pages/index/requst/cdetail/cdetail.js
@@ -1,83 +1,66 @@
-const app=getApp();
-
+// pages/index/requst/cdetail/cdetail.js
 Page({
 
-  onItemClick:function(){
-    wx.navigateTo({
-      url: 'cdetail/cdetail',
-    })
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-    wx.showLoading({
-      title: '加载中',
-    })
-    var that=this;
-    const requestTask =wx.request({
-      url: 'http://gank.io/api/xiandu/category/android',
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      success: function (res) {
-        that.setData({
-          android: res.data.results
-        })
-        wx.hideLoading()
-      },
-    })
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+  
   }
 })
