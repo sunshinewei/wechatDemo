2b0b4d5e91e02606a8931668bce2693bf6be674e
sunshienwei <1436699184@qq.com>
2018-07-27 18:09:18 +0800

    topbar

--- pages/tabme/tabme.js
+++ pages/tabme/tabme.js
@@ -1,4 +1,4 @@
-// pages/index/requst/cdetail/cdetail.js
+// pages/tabme/tabme.js
 Page({
 
   /**
@@ -12,9 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      url:options.url
-    })
+  
   },
 
   /**
--- pages/tabtwo/tabtwo.js
+++ pages/tabtwo/tabtwo.js
@@ -1,59 +1,43 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/tabtwo/tabtwo.js
 Page({
 
-  onItemClick: function () {
-    wx.navigateTo({
-      url: 'requst/cdetail/cdetail',
-    })
-  },
-
-/** 
- * 下拉刷新
-*/
-  onPullDownRefresh:function(Msg){
-    wx.showNavigationBarLoading() //在标题栏中显示加载
-    var that = this;
-    wx.request({
-      url: 'http://gank.io/api/random/data/Android/20',
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      success: function (res) {
-        that.setData({
-          android: res.data.results
-        })
-
-        wx.stopPullDownRefresh()
-      },
-    })
+  /**
+   * 页面的初始数据
+   */
+  data: {
   
   },
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
+    var that=this;
+
     wx.showLoading({
       title: '加载中',
     })
-    var that = this;
-    const requestTask = wx.request({
-      url: 'http://gank.io/api/random/data/Android/20',
-      method: 'GET',
+
+    wx.request({
+      url: 'http://gank.io/api/data/福利/10/1',
+      method:'GET',
       headers: {
         'Content-Type': 'application/json'
       },
-      success: function (res) {
+
+      success:function(result){
+
         that.setData({
-          android: res.data.results
+
+          data: result.data.results
         })
+        
         wx.hideLoading()
-      },
+      }
+
     })
   },
 
@@ -61,48 +45,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
